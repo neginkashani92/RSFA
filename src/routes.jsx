95103ed0fd59f3e050107c18be78635f2f57db73
Negin Kashani <negin.b.kashany@gmail.com>
2025-07-30 14:10:53 -0400

    React router added

---
+++ src/routes.jsx
@@ -0,0 +1,14 @@
+import { Children } from "react";
+import App from "./App";
+
+const routes=[
+    {
+        Path:'/',
+        Element:
+        errorElement:
+        Children:[
+
+        ]
+    }
+]
+export default routes
