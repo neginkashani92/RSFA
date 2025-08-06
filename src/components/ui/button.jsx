268611143c7fdd96e377b6a324b895628e3c7ff3
Negin Kashani <negin.b.kashany@gmail.com>
2025-08-06 14:29:42 -0400

    Implement forms for user detail view, password reset, and email update

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -10,7 +10,9 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-navy  rounded-none text-white shadow-xs hover:bg-navy/80",
+        primary:
+          "bg-navy opacity-50 rounded-none text-white",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
